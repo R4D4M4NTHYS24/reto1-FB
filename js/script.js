6261efdd7a8ad37c13ea952328f3d9e5ae6cefee
Camilo Restrepo <camilorg2017@gmail.com>
2019-09-05 12:19:01 -0500

    Refactor del codigo consumo API
    Se actualiza el codigo para que consuma la API en cada elemento presente del index y tambien se crea conexion con la seccion details del site

--- js/script.js
+++ js/script.js
@@ -3,62 +3,49 @@ let characterPosition = 0;
 let userImage = document.querySelector("#im1");
 let characters = [];
 let character = {};
+let elements;
 
 function userInfo() {
     fetch('https://rickandmortyapi.com/api/character').then((respuesta) => {
         return respuesta.json();
     })
     .then((respuesta) => {
-        /*console.log(respuesta);
-        characters.push(respuesta.results);
-        console.log(respuesta.info.next);
-        fetch('https://rickandmortyapi.com/api/character/?page=16').then((respuesta)=>{
-            return respuesta;
-        })
-        characters.push(respuesta.results);
-        console.log(respuesta);*/
-        length = respuesta.results.length;        
-        characterPosition = Math.floor(Math.random()*length + 1)-1;
-        character = respuesta.results[characterPosition];
-        userImage.src = character.image;
+        
+        serviceLength = respuesta.results.length;        
+        elements = document.getElementsByClassName('post');
+        for (let index = 0; index < elements.length; index++) {
+            let element = document.getElementsByClassName('image')[index];
+            characterPosition = Math.floor(Math.random() * serviceLength + 1) - 1;
+            character = respuesta.results[characterPosition];
+            element.src = character.image;
+
+            
+            
+            
+        }
+        //console.log("hola")
         localStorage.setItem('character', JSON.stringify(character));
+        /*
+        let detectar = document.getElementsByClassName('post-image');       
+
+            detectar.addEventListener('click', function(){
+                localStorage.setItem('character', JSON.stringify(character));
+            })*/
+        
+
+        
+
+        
 
 
-        /*userImage.src = 'https://rickandmortyapi.com/api/character/avatar/' + random.toString() +'.jpeg';
-        localStorage.setItem('random1', random);
-        userImage.addEventListener('click', change(1));
 
-        userImage = document.querySelector("#im2");
-        random = Math.floor(Math.random()*length + 1);
-        userImage.src = 'https://rickandmortyapi.com/api/character/avatar/' + random.toString() +'.jpeg';
-        localStorage.setItem('random2', random);
-        userImage.addEventListener('click', change(2));
-
-        userImage = document.querySelector("#im3");
-        random = Math.floor(Math.random()*length + 1);
-        userImage.src = 'https://rickandmortyapi.com/api/character/avatar/' + random.toString() +'.jpeg';
-        localStorage.setItem('random3', random);
-        userImage.addEventListener('click', change(3));*/
     })
+    
 }
-userInfo();
-//change();
 
-function change(number) {
-    console.log('->',number);
-    if(number == 1){
-        //console.log(localStorage.getItem('random1'));
-        localStorage.setItem('random2', null);
-        localStorage.setItem('random3', null);
-    }
-    else if (number== 2){
-        //console.log(localStorage.getItem('random2'));
-        localStorage.setItem('random1', null);
-        localStorage.setItem('random3', null);
-    }
-    else if (number== 3){
-        localStorage.setItem('random1', null);
-        localStorage.setItem('random2', null);
-        //console.log(localStorage.getItem('random3'));
-    }
-}	
+userInfo();
+
+
+
+
+	
--- js/script2.js
+++ js/script2.js
@@ -3,6 +3,24 @@ let userDetails = document.querySelector(".character-details");
 let userLocalObject = localStorage.getItem("character");
 let userObject = JSON.parse(userLocalObject);
 let li = document.createElement("li");
+
+console.log(localStorage.getItem("random1"));
+console.log(localStorage.getItem("random2"));
+console.log(localStorage.getItem("random3"));
+
 li.innerHTML = userObject.name;
 userDetails.appendChild(li);
 userImage.src = userObject.image;
+
+li = document.createElement("li");
+li.innerHTML = userObject.status;
+userDetails.appendChild(li);
+
+li = document.createElement("li");
+li.innerHTML = userObject.species;
+userDetails.appendChild(li);
+
+li = document.createElement("li");
+li.innerHTML = userObject.gender;
+userDetails.appendChild(li);
+
