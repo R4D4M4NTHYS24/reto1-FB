5c9f8265d0f5af71f3fc6e5016683106e69f517f
Camilo Restrepo <camilorg2017@gmail.com>
2019-09-07 12:40:40 -0500

    Refactor en la funcion fetch de la API
    Se hace un refactor que permite traer aleatoriamente cualquiera de los 493 personajes al azar en la seccion de index.
    Tambien se crea la coneccion con la seccion de details de cada personaje.

--- js/script.js
+++ js/script.js
@@ -3,28 +3,43 @@ let characterPosition = 0;
 let userImage = document.querySelector("#im1");
 let characters = [];
 let character = {};
-let elements;
+let postElements;
+let characterPosition2;
+let characterPosition3;
 
 function userInfo() {
-    fetch('https://rickandmortyapi.com/api/character').then((respuesta) => {
+    characterPosition = Math.floor(Math.random() * 493 + 1) - 1;
+    characterPosition2 = Math.floor(Math.random() * 493 + 1) - 1;
+    characterPosition3 = Math.floor(Math.random() * 493 + 1) - 1;
+    fetch(`https://rickandmortyapi.com/api/character/[${characterPosition},${characterPosition2},${characterPosition3}]`).then((respuesta) => {
         return respuesta.json();
     })
     .then((respuesta) => {
         
-        serviceLength = respuesta.results.length;        
-        elements = document.getElementsByClassName('post');
-        for (let index = 0; index < elements.length; index++) {
-            let element = document.getElementsByClassName('image')[index];
-            characterPosition = Math.floor(Math.random() * serviceLength + 1) - 1;
-            character = respuesta.results[characterPosition];
-            element.src = character.image;
+            
+        postElements = document.getElementsByClassName('post');
+        for (let index = 0; index < postElements.length; index++) {
+            //let element = document.getElementsByClassName('image')[index];
+            let postImage = postElements[index].querySelector('.image');
+            
+            character = respuesta[index];
+            postImage.src = character.image;
+            
+            postElements[index].addEventListener('click', function(){
+                console.log("entre");
+                console.log(index);
+                console.log(character);
+                localStorage.setItem('character', JSON.stringify(respuesta[index]));
+            });
+            
+            
 
             
             
             
         }
-        //console.log("hola")
-        localStorage.setItem('character', JSON.stringify(character));
+        
+        
         /*
         let detectar = document.getElementsByClassName('post-image');       
 
